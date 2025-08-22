5db574e339c8502b7604cab69267da9e158da5b2
janhavi73 <janhavisinghania0607@gmail.com>
2025-08-22 10:30:52 +0100

    Add files via upload

---
+++ rides.py
@@ -0,0 +1,13 @@
+print("please select your ride:")
+print("1. Bike")
+print("2. Car")
+choice=int(input("please enter your choice:"))
+if (choice ==1):
+    print("what type of bike?")
+    print("1.Scooty\n")
+    print("2. Scooter\n")
+
+    choice2=(input("please enter you choice2"))
+    if choice2 == 1:
+        
+
